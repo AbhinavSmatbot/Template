a08c3f464f21df832b5c477adad8753f2156a93b
AbhinavSmatbot <Abhinav.smatbot@gmail.com>
2023-11-17 17:10:30 +0530

    'language-json-added'

--- src/app/store.jsx
+++ src/app/store.jsx
@@ -9,5 +9,4 @@ export const store = configureStore({
      header:HeaderSlice,
      body:BodyAndFooterSlice,
   }
-
 })
--- src/components/Home/Firstmain.jsx
+++ src/components/Home/Firstmain.jsx
@@ -2,16 +2,52 @@
 /* eslint-disable react/prop-types */
 
 import { useEffect, useState } from 'react';
-import {FormControl,OutlinedInput,InputLabel,MenuItem,Select,Chip,Radio} from '@mui/material';
+import {FormControl,OutlinedInput,InputLabel,MenuItem,Select,Chip,Radio, NativeSelect} from '@mui/material';
 import { Link } from 'react-router-dom';
 import { AiTwotoneSound } from 'react-icons/ai';
 import { BiSolidBellRing } from 'react-icons/bi';
 import { FaKey } from 'react-icons/fa';
 import { AiOutlineSearch } from 'react-icons/ai';
-
+import {All_languages} from './Templates_languages';
 import { useDispatch, useSelector } from 'react-redux';
 import { updateCategory,upadateTemName,upadateLanguage } from '../../app/features/HomeSlice';
+import InputBase from '@mui/material/InputBase';
+import { styled } from '@mui/material/styles';
 // import FormHelperText from '@mui/material/FormHelperText';
+
+const BootstrapInput = styled(InputBase)(({ theme }) => ({
+     'label + &': {
+       marginTop: theme.spacing(3),
+     },
+     '& .MuiInputBase-input': {
+       borderRadius: 4,
+       position: 'relative',
+       backgroundColor: theme.palette.background.paper,
+       border: '1px solid #ced4da',
+       fontSize: 16,
+       padding: '10px 26px 10px 12px',
+       transition: theme.transitions.create(['border-color', 'box-shadow']),
+       // Use the system font instead of the default Roboto font.
+       fontFamily: [
+         '-apple-system',
+         'BlinkMacSystemFont',
+         '"Segoe UI"',
+         'Roboto',
+         '"Helvetica Neue"',
+         'Arial',
+         'sans-serif',
+         '"Apple Color Emoji"',
+         '"Segoe UI Emoji"',
+         '"Segoe UI Symbol"',
+       ].join(','),
+       '&:focus': {
+         borderRadius: 4,
+         borderColor: '#80bdff',
+         boxShadow: '0 0 0 0.2rem rgba(0,123,255,.25)',
+       },
+     },
+   }));
+
 
 const ITEM_HEIGHT = 48;
 const ITEM_PADDING_TOP = 8;
@@ -268,7 +304,7 @@ const Firstmain = (props) => {
                              <div>
                                    <FormControl className='w-full text-sm font-Secondary'>
                                    {languages?.length == 0 && <InputLabel id="demo-multiple-chip-label">Select languages</InputLabel>}
-                                   <Select className='min-h-[48px] text-sm'
+                                   <Select className=' text-sm'
                                         labelId="demo-multiple-chip-label"
                                         id="demo-multiple-chip"
                                         multiple
@@ -298,13 +334,13 @@ const Firstmain = (props) => {
                                         MenuProps={MenuProps}
                                    >
                                         
-                                        {names.map((name) => (
-                                        <MenuItem
-                                        key={name}
-                                        value={name}
-                                        style={getStyles(name, languages)}
+                                        {All_languages.map((item) => (
+                                        <MenuItem 
+                                        key={item.value}
+                                        value={item.name}
+                                        style={{...getStyles(item.name, languages),fontSize:"14px"}}
                                         >
-                                        {name}
+                                        {item.name}
                                         </MenuItem>
                                         ))}
                                    </Select>
